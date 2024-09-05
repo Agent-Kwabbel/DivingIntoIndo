1cc01f8bff43c160a673403d12779f3700719b8f
Agent-Kwabbel <fabestuffken@proton.me>
2024-09-05 23:12:44 +0200

    Better navigation

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,10 +3,12 @@ import Link from "next/link";
 export function Footer() {
     return (
         <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-            <p className="text-xs text-muted-foreground">&copy; 2024 Indonesia Travel Blog. All rights reserved.</p>
-            <nav className="sm:ml-auto flex gap-4 sm:gap-6">
-                <Link href="#" className="text-xs hover:underline underline-offset-4">Terms of Service</Link>
-                <Link href="#" className="text-xs hover:underline underline-offset-4">Privacy</Link>
+            <p className="text-xs text-muted-foreground">&copy; 2024 Diving Into Indo. All rights reserved.</p>
+            <nav className="sm:ml-auto flex items-center gap-4 sm:gap-6">
+                <Link href="/terms-of-service" className="text-xs hover:underline underline-offset-4">Terms of Service</Link>
+                <Link href="/privacy" className="text-xs hover:underline underline-offset-4">Privacy</Link>
+                <div className="border-r-2 border-muted-foreground-foreground h-5" />
+                <Link href="https://github.com/Agent-Kwabbel/DivingIntoIndo" target="_blank" className="text-xs hover:underline underline-offset-4">GitHub</Link>
             </nav>
         </footer>
     );
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,19 +6,22 @@ import { MenuIcon, HomeIcon, BookIcon, UserIcon, MailIcon, MountainIcon } from "
 
 export function Navigation() {
     return (
-        <header className="px-4 lg:px-6 h-14 flex items-center">
-            <Link href="#" className="flex items-center justify-center" prefetch={false}>
+        <header className="px-4.5 lg:px-7 h-14 flex items-center">
+            <Link href="/" className="flex items-center justify-center" prefetch={false}>
                 <MountainIcon className="h-6 w-6"/>
                 <span className="sr-only">Adventure Blog</span>
             </Link>
             <nav className="ml-auto flex gap-4 sm:gap-6">
-                <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+                <Link href="/blog" className="text-base font-medium hover:underline underline-offset-4" prefetch={false}>
                     Blog
                 </Link>
-                <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+                <Link href="/trip" className="text-base font-medium hover:underline underline-offset-4" prefetch={false}>
+                    Trip
+                </Link>
+                <Link href="/about" className="text-base font-medium hover:underline underline-offset-4" prefetch={false}>
                     About
                 </Link>
-                <Link href="#" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
+                <Link href="/contact" className="text-base font-medium hover:underline underline-offset-4" prefetch={false}>
                     Contact
                 </Link>
             </nav>
