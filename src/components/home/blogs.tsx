e8441483e5610cf02db3c766f03de6bab87e014c
Agent-Kwabbel <fabestuffken@proton.me>
2024-09-07 13:49:48 +0200

    Improve blogs section on home page

--- src/components/home/blogs.tsx
+++ src/components/home/blogs.tsx
@@ -24,8 +24,13 @@ export function BlogSection() {
 
     return (
         <section className="w-full py-10 lg:pb-12 bg-muted">
+            <div className="w-full h-full flex items-center justify-center">
             <div className="container px-4 md:px-6">
-                <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <Link href="/blog" className="mb-6 font-heading inline-flex items-center gap-1 text-xl md:text-3xl font-semibold hover:underline">
+                    Latest Blog Posts
+                    <ArrowRightIcon className="h-6 w-6 md:h-8 md:w-8" />
+                </Link>
+                <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                     {posts.map((post, index) => (
                         <Card key={index}>
                             <CardContent className="space-y-2">
@@ -40,6 +45,7 @@ export function BlogSection() {
                         </Card>
                     ))}
                 </div>
+            </div>
             </div>
         </section>
     );
