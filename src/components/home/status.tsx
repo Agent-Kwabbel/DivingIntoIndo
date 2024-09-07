2c6c3bb961ce163c75d52e90f2cc43e08542bcf2
Agent-Kwabbel <fabestuffken@proton.me>
2024-09-07 22:50:53 +0200

    Clean up status section code

--- src/components/home/status.tsx
+++ src/components/home/status.tsx
@@ -58,13 +58,13 @@ export function StatusSection() {
     );
 }
 
-function Circle(status: { status: StatusType }) {
+function Circle({ status }: { status: StatusType }) {
     return (
       <span className="relative flex h-4 w-4">
-          {statusOptions[status.status].ping && (
-            <span className={`absolute animate-ping inline-flex h-full w-full rounded-full ${statusOptions[status.status].color}`}></span>
+          {statusOptions[status].ping && (
+            <span className={`absolute animate-ping inline-flex h-full w-full rounded-full ${statusOptions[status].color}`}></span>
           )}
-          <span className={`relative inline-flex rounded-full h-4 w-4 ${statusOptions[status.status].color}`} title="Is the status circle pusling? Than the trip is ongoing!"></span>
+          <span className={`relative inline-flex rounded-full h-4 w-4 ${statusOptions[status].color}`} title="Is the status circle pusling? Than the trip is ongoing!"></span>
       </span>
     );
 }
